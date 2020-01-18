0cafbab0d71e180bacbe4248e9a27453f2128ceb
Seohyun Lee <04seohyun@gmail.com>
2020-01-18 17:14:24 +0900

    3568_Solve_4

--- 3568.cpp
+++ 3568.cpp
@@ -7,64 +7,55 @@
 using namespace std;
 
 struct node {
-    node *l, *r;
+    int l, r;
     int val;
-
-    node() {}
-
-    node(int _v) {
-        val = _v;
-    }
-
-    node(int _v, node *_l, node *_r) {
-        val = _v;
-        l = _l;
-        r = _r;
-    }
 };
 
-node *createNode(node *l, node *r) {
-    if (l == nullptr) return new node(r->val, l, r);
-    if (r == nullptr) return new node(l->val, l, r);
-    return new node(l->val + r->val, l, r);
+int n, h, q, n2 = 1, unc, tar, l, r;
+int root[1000002];
+node nodes[23000005];
+int his[1000002], db[1000002];
+
+int createNode(int l, int r) {
+    nodes[++unc].l = l;
+    nodes[unc].r = r;
+    if (l != -1) nodes[unc].val = nodes[l].val;
+    if (r != -1) nodes[unc].val += nodes[r].val;
+    return unc;
 }
 
-node *createNode(int v) {
-    return new node(v);
+int createNode(int v) {
+    nodes[++unc].val = v;
+    return unc;
 }
 
-int n, h, q, n2 = 1;
-vector<int> v;
-vector<node *> root;
-vector<int> his;
-vector<int> db;
-
-node *createTree(int tar, node *laTree, int nh = 0) {
-    if (laTree == nullptr) return nullptr;
-    if (nh == h) return createNode((laTree->val) + 1);
-    node *nl = laTree->l, *nr = laTree->r;
-    if (tar & (1 << (h - nh - 1))) nr = createTree(tar, laTree->r, nh + 1);
-    else nl = createTree(tar, laTree->l, nh + 1);
-    return createNode(nl, nr);
-}
-
-void updTree(int tar, node *tree, int nh = 0) {
-    if (nh == h) {
-        tree->val = tree->val + 1;
-        return;
+int createFirstTree(int l = 0, int r = n2) {
+    if (l == r) {
+        if (l >= n) return -1;
+        if (l == tar) return createNode(1);
+        else return createNode(0);
     }
-    if (tar & (1 << (h - nh - 1))) updTree(tar, tree->r, nh + 1);
-    else updTree(tar, tree->l, nh + 1);
-    tree->val = (tree->l->val) + (tree->r->val);
+    int lt = createFirstTree(l, (l + r) / 2), rt = createFirstTree((l + r) / 2 + 1, r);
+    if (lt == -1 && rt == -1) return -1;
+    return createNode(lt, rt);
 }
 
-int queryTree(int l, int r, node *tree, int tl = 0, int tr = n2) {
+int createTree(int laTree, int nh = 0) {
+    if (laTree == -1) return -1;
+    if (nh == h) return createNode((nodes[laTree].val) + 1);
+    if (tar & (1 << (h - nh - 1))) return createNode(nodes[laTree].l, createTree(nodes[laTree].r, nh + 1));
+    else return createNode(createTree(nodes[laTree].l, nh + 1), nodes[laTree].r);
+}
+
+int queryTree(int tree, int tl = 0, int tr = n2) {
     // 1<<(h-nh-1)
+    if (tree == -1) return 0;
     if (tr < l) return 0;
     if (r < tl) return 0;
-    if (l <= tl && r >= tr) return tree->val;
-    return queryTree(l, r, tree->l, tl, (tl + tr) / 2) + queryTree(l, r, tree->r, (tl + tr) / 2 + 1, tr);
+    if (l <= tl && r >= tr) return nodes[tree].val;
+    return queryTree(nodes[tree].l, tl, (tl + tr) / 2) + queryTree(nodes[tree].r, (tl + tr) / 2 + 1, tr);
 }
+
 
 int main() {
     scanf("%d %d", &n, &q);
@@ -75,38 +66,32 @@ int main() {
     }
     while (n > n2) n2 <<= 1;
     --n2;
-    v.resize(n);
-    his.resize(n + 2);
-    db.resize(n + 1);
+    int t;
     for (int i = 0; i < n; ++i) {
-        scanf("%d", &v[i]);
-        if (his[v[i]]) db[his[v[i]] - 1] = i;
-        his[v[i]] = i + 1;
+        scanf("%d", &t);
+        if (his[t]) db[his[t] - 1] = i;
+        his[t] = i + 1;
     }
-    vector<node *> tv, nv;
-    for (int i = 0; i < n; ++i) nv.push_back(createNode((i) && (i == db[0])));
-    while (nv.size() > 1) {
-        tv = nv;
-        nv.clear();
-        for (int i = 0; i < tv.size() / 2 * 2; i += 2) nv.push_back(createNode(tv[i], tv[i + 1]));
-        if (tv.size() % 2) nv.push_back(createNode(tv.back(), nullptr));
-    }
-    root.push_back(nv[0]);
+    tar = db[0]-1;
+    root[0] = createFirstTree();
     for (int i = 1; i <= n; ++i) {
         if (db[i] == 0) {
-            root.push_back(root.back());
+            root[i] = root[i - 1];
             continue;
         }
-        root.push_back(createTree(db[i], root.back()));
+        tar = db[i];
+        root[i] = createTree(root[i - 1]);
     }
     for (int i = 0; i < q; ++i) {
         int a, b;
         scanf("%d %d", &a, &b);
         --a;
         --b;
+        l = a;
+        r = b;
         if (!a) {
             if (!b)printf("%d\n", (b - a + 1));
-            else printf("%d\n", (b - a + 1) - (queryTree(a, b, root[b])));
-        } else printf("%d\n", (b - a + 1) - (queryTree(a, b, root[b]) - queryTree(a, b, root[a - 1])));
+            else printf("%d\n", (b - a + 1) - (queryTree(root[b])));
+        } else printf("%d\n", (b - a + 1) - (queryTree(root[b]) - queryTree(root[a - 1])));
     }
 }
